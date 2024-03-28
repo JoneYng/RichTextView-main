e5a77f86d4950e4a9a42b75780ee019c2d93ad15
zhouxiang <zhou_xiang99@163.com>
2024-03-28 15:56:34 +0800

    feat:支持span标签

---
+++ app/src/main/java/com/example/tablerichtext/htmltext/ColorToRgb.kt
@@ -0,0 +1,45 @@
+package com.example.tablerichtext.htmltext
+
+import android.content.Context
+import java.util.Locale
+
+
+/**
+ * @description:
+ * @author: zhouxiang
+ * @created: 2024/03/28 09:56
+ * @version: V1.0
+ */
+class ColorToRgb {
+    companion object {
+        fun toRgb(colorRgb:String):String{
+            var color=colorRgb
+            if (color.startsWith("rgb")) {
+                color = color.replace("rgb(", "")
+                color = color.replace(")", "")
+                val rgbs = color.split(", ")
+                if (rgbs.size > 2) {
+                    color = toHex(rgbs[0].toInt(), rgbs[1].toInt(), rgbs[2].toInt())
+                }
+            }
+            return color
+        }
+
+         fun toHex(r : Int, g : Int, b : Int) : String {
+            return "#" + toBrowserHexValue(r) + toBrowserHexValue(g) + toBrowserHexValue(b)
+        }
+
+         fun toBrowserHexValue(number: Int) : String {
+            val builder = StringBuilder(number.and(0xff).toString(16))
+            while (builder.length < 2) {
+                builder.append("0")
+            }
+            return builder.toString().uppercase(Locale.getDefault())
+        }
+
+         fun sp2px(context: Context, pxValue: Int) : Int {
+            val fontScale = context.resources.displayMetrics.scaledDensity
+            return (pxValue * fontScale + 0.5f).toInt()
+        }
+   }
+}
